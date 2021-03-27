cc7e3bf4ef28ad5c3614387db64e69566e78aa3c
Michelle Pine <michelle.pine33@gmail.com>
2021-03-26 21:55:27 -0400

    suggestions

--- frontend/src/components/Chat/ChatSidebar.tsx
+++ frontend/src/components/Chat/ChatSidebar.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
-import ChatSelectTabs from './ChatSelectTabs';
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import ChatContainer from './ChatContainer';
 import './ChatSidebar.css';
+import DirectMessageSelect from './DirectMessageSelect';
 
 export default function ChatSidebar(): JSX.Element {
+  const [isViewingDirectChatContainer, setIsViewingDirectChatContainer] = useState<boolean>(false);
+
+  const handleReturnToSelect = () => {
+    if (isViewingDirectChatContainer) {
+      setIsViewingDirectChatContainer(false);
+    }
+  };
+
+  const directMessageTab = isViewingDirectChatContainer ? 'All Direct Chats' : 'Direct Chats';
+
   return (
     <div className='chat-sidebar'>
       <div className='sidebar-header'>Chat</div>
-      <ChatSelectTabs/>
+      <Tabs isLazy isFitted variant='enclosed' defaultIndex={0} size='md'>
+        <TabList mb='1em'>
+          <Tab onClick={isViewingDirectChatContainer ? handleReturnToSelect : undefined}>
+            {directMessageTab}
+          </Tab>
+          <Tab>Town Chat</Tab>
+          <Tab>Proximity Chat</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <DirectMessageSelect
+              setIsViewingChatContainer={setIsViewingDirectChatContainer}
+              isViewingChatContainer={isViewingDirectChatContainer}
+            />
+          </TabPanel>
+          <TabPanel>
+            <ChatContainer />
+          </TabPanel>
+          <TabPanel>
+            <ChatContainer />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </div>
   );
 }
