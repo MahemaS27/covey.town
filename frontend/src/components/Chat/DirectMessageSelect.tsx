cbc79d07f9f0e8106384ccd82eece8c52cbdd504
Michelle Pine <32271661+michelle-pine@users.noreply.github.com>
2021-03-27 12:21:37 -0400

    suggestions (#13)

--- frontend/src/components/Chat/DirectMessageSelect.tsx
+++ frontend/src/components/Chat/DirectMessageSelect.tsx
@@ -1,28 +1,31 @@
-import React, { useState } from 'react';
 import { Box, Button } from '@chakra-ui/react';
+import React from 'react';
 // import useCoveyAppState from '../../hooks/useCoveyAppState';
 import ChatContainer from './ChatContainer';
 
-export default function DirectMessageSelect(): JSX.Element {
-    const [isViewingChatContainer, setIsViewingChatContainer] = useState<boolean>(false);
-
-    console.log(isViewingChatContainer);
-    if (isViewingChatContainer === true) {
-        return <ChatContainer />;
-    }
-    return (<div>
-        Started:
-        <Box>
-            userName
-            <Button onClick={() => setIsViewingChatContainer(true)}>Continue Chat</Button>
-        </Box>
-
-        Not Started:
-        <Box>
-            userName2
-            <Button onClick={() => setIsViewingChatContainer(true)}>Start Chat</Button>
-        </Box>
-    </div>
-    );
+interface DirectMessageSelectProps {
+  isViewingChatContainer: boolean;
+  setIsViewingChatContainer: (isViewingChatContainer: boolean) => void;
 }
-
+export default function DirectMessageSelect({
+  isViewingChatContainer,
+  setIsViewingChatContainer,
+}: DirectMessageSelectProps): JSX.Element {
+  if (isViewingChatContainer) {
+    return <ChatContainer />;
+  }
+  return (
+    <div>
+      Started:
+      <Box>
+        userName
+        <Button onClick={() => setIsViewingChatContainer(true)}>Continue Chat</Button>
+      </Box>
+      Not Started:
+      <Box>
+        userName2
+        <Button onClick={() => setIsViewingChatContainer(true)}>Start Chat</Button>
+      </Box>
+    </div>
+  );
+}
