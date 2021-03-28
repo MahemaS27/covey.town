02eea66bd5fc64c518d72cb0772ffe18ee2fcf5d
Abigail Hodge <abigailhodge98@gmail.com>
2021-03-28 19:50:11 -0400

    Added test for GameController

--- frontend/src/gameController.test.ts
+++ frontend/src/gameController.test.ts
@@ -1,8 +1,46 @@
-import { mock, mockReset,  } from 'jest-mock-extended';
-import { nanoid } from 'nanoid';
-import { Socket } from 'socket.io-client';
-import { GameController } from './GameController';
+import * as socket from 'socket.io-client';
 
-describe('game controller', () => {
-    const mockSocket = mock<Socket>();
-})
+import { Socket } from 'socket.io-client';
+import { mock, mockReset } from 'jest-mock-extended';
+import { nanoid } from 'nanoid';
+import { GameController } from './GameController';
+import Video from './classes/Video/Video';
+import { createMessageForTesting } from './TestUtils';
+import { MessageType } from './classes/MessageChain';
+jest.mock('./classes/Video/Video');
+
+jest.mock('socket.io-client', () => ({
+    io: () => ({
+      on: jest.fn(),
+      emit: jest.fn(),
+    }),
+  }));
+
+  const mockVideoSetup = mock<Video>();
+    
+  Video.instance = () => {
+      return mockVideoSetup;
+  };
+
+it('emits a message to the socket when a message is sent', async () => {
+    const dispatchAppUpdate = jest.fn();
+    const initData = {
+        coveyUserID: "id",
+        coveySessionToken: "token",
+        providerVideoToken: "vid token",
+        currentPlayers: [],
+        friendlyName: "test town 123",
+        isPubliclyListed: true,
+    }  
+    
+    await GameController(initData, dispatchAppUpdate);
+    
+    const dispatchAppData = dispatchAppUpdate.mock.calls[0][0].data;
+    const emitMessage = dispatchAppData.emitMessage;
+    console.log(emitMessage)
+    const socket = dispatchAppData.socket;
+    const message = createMessageForTesting(MessageType.TownMessage, nanoid());
+
+    emitMessage(message);
+    expect(socket.emit).toBeCalledWith('messageSent', message);
+});
