10acb8f543f887250ceb5414e29c6cb37e7e831b
Abigail Hodge <abigailhodge98@gmail.com>
2021-03-28 23:21:56 -0400

    Setting env vars for testing

--- frontend/src/gameController.test.ts
+++ frontend/src/gameController.test.ts
@@ -1,46 +1,59 @@
-import * as socket from 'socket.io-client';
-
-import { Socket } from 'socket.io-client';
-import { mock, mockReset } from 'jest-mock-extended';
+import { mock } from 'jest-mock-extended';
 import { nanoid } from 'nanoid';
-import { GameController } from './GameController';
-import Video from './classes/Video/Video';
-import { createMessageForTesting } from './TestUtils';
 import { MessageType } from './classes/MessageChain';
+import Video from './classes/Video/Video';
+import { GameController } from './GameController';
+import { createMessageForTesting } from './TestUtils';
+
 jest.mock('./classes/Video/Video');
 
 jest.mock('socket.io-client', () => ({
-    io: () => ({
-      on: jest.fn(),
-      emit: jest.fn(),
-    }),
-  }));
+  io: () => ({
+    on: jest.fn(),
+    emit: jest.fn(),
+  }),
+}));
 
-  const mockVideoSetup = mock<Video>();
-    
-  Video.instance = () => {
-      return mockVideoSetup;
-  };
+const mockVideoSetup = mock<Video>();
 
-it('emits a message to the socket when a message is sent', async () => {
+Video.instance = () => {
+  return mockVideoSetup;
+};
+
+describe('game controller', () => {
+  // from https://stackoverflow.com/questions/48033841/test-process-env-with-jest
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules(); // Most important - it clears the cache
+    process.env = { ...OLD_ENV }; // Make a copy
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV; // Restore old environment
+  });
+
+  it('emits a message to the socket when a message is sent', async () => {
+    process.env.REACT_APP_TOWNS_SERVICE_URL = 'testurl.com';
     const dispatchAppUpdate = jest.fn();
     const initData = {
-        coveyUserID: "id",
-        coveySessionToken: "token",
-        providerVideoToken: "vid token",
-        currentPlayers: [],
-        friendlyName: "test town 123",
-        isPubliclyListed: true,
-    }  
-    
+      coveyUserID: 'id',
+      coveySessionToken: 'token',
+      providerVideoToken: 'vid token',
+      currentPlayers: [],
+      friendlyName: 'test town 123',
+      isPubliclyListed: true,
+    };
+
     await GameController(initData, dispatchAppUpdate);
-    
+
     const dispatchAppData = dispatchAppUpdate.mock.calls[0][0].data;
     const emitMessage = dispatchAppData.emitMessage;
-    console.log(emitMessage)
+    console.log(emitMessage);
     const socket = dispatchAppData.socket;
     const message = createMessageForTesting(MessageType.TownMessage, nanoid());
 
     emitMessage(message);
     expect(socket.emit).toBeCalledWith('messageSent', message);
+  });
 });
