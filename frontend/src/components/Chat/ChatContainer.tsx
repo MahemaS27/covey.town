3a1675123cd0eef373cb0e28078b45b716d812df
anikarabenhorst <anikacchase@gmail.com>
2021-04-01 19:33:03 -0400

    feat: connect to right chain and display

--- frontend/src/components/Chat/ChatContainer.tsx
+++ frontend/src/components/Chat/ChatContainer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Message, MessageType } from '../../classes/MessageChain';
+import MessageChain, { Message, MessageType } from '../../classes/MessageChain';
+import useCoveyAppState from '../../hooks/useCoveyAppState';
 import './ChatContainer.css';
 import ChatInput from './ChatInput';
 import SingleMessage from './SingleMessage';
@@ -21,7 +22,7 @@ const sampleMessage: Message = {
   },
   directMessageId: null,
 };
- 
+
 interface ChatContainerProps {
   // so for now- Town/Proximity will pass in undefined
   directMessageID: string | null;
@@ -35,30 +36,42 @@ export default function ChatContainer({
   // optional passing of an direct message chain ID, depends on chain type
   // if new DM - wait until first input to create the new DM chain -- using the id that was given as a prop
   // eventually useCoveyAppState to connect to messages
+  const { myPlayerID, directMessageChains, townMessageChain, proximityMessageChain } = useCoveyAppState();
+  let displayedChain: MessageChain | undefined;
+  switch (chainType) {
+    case 'DirectMessage': {
+      if (directMessageID) {
+        displayedChain = directMessageChains[directMessageID];
+      }
+      break;
+    }
+    case 'TownMessage': {
+      displayedChain = townMessageChain;
+      break;
+    }
+    default: {
+      displayedChain = proximityMessageChain;
+    }
+  }
 
-  const finalSentence =
-    chainType === MessageType.DirectMessage ? `you want ${directMessageID}` : '';
-  return (
-    <div className='chat-container'>
-      <div className='scrollable-messages'>
-        {/* delete these when real messages replace them */}
-        Chat type is {chainType}. No one has sent a message yet. {finalSentence}
-        <SingleMessage message={sampleMessage} myPlayerID='23124' />
-        <SingleMessage message={sampleMessage} myPlayerID='122342' />
-        <SingleMessage message={sampleMessage} myPlayerID='33333' />
-        <SingleMessage message={sampleMessage} myPlayerID='123234' />
-        <SingleMessage message={sampleMessage} myPlayerID='122325' />
-        <SingleMessage message={sampleMessage} myPlayerID='33333' />
-        <SingleMessage message={sampleMessage} myPlayerID='123235' />
-        <SingleMessage message={sampleMessage} myPlayerID='33333' />
-      </div>
-      <div className='chat-input'>
-        <ChatInput
-          messageType={chainType}
-          directMessageId={directMessageID}
-          isDisabled={undefined}
-        />
-      </div>
+  if (!displayedChain) {
+    return <div className='chat-container'>
     </div>
-  );
+  }
+  return <div className='chat-container'>
+    <div className='scrollable-messages'>
+      {
+        displayedChain.messages.map((message) => (
+          <SingleMessage message={message} myPlayerID={myPlayerID} />
+        ))
+      }
+    </div>
+    <div className='chat-input'>
+      <ChatInput
+        messageType={chainType}
+        directMessageId={directMessageID}
+        isDisabled={undefined}
+      />
+    </div>
+  </div>;
 }
