64272f28ee03257bc8cecbe1423ebbfac7f11d77
anikarabenhorst <anikacchase@gmail.com>
2021-04-02 21:20:54 -0400

    style fixes

--- frontend/src/components/Chat/ChatContainer.tsx
+++ frontend/src/components/Chat/ChatContainer.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
-import MessageChain, { Message, MessageType } from '../../classes/MessageChain';
+import MessageChain, { MessageType } from '../../classes/MessageChain';
 import useCoveyAppState from '../../hooks/useCoveyAppState';
 import './ChatContainer.css';
 import ChatInput from './ChatInput';
 import SingleMessage from './SingleMessage';
-
-// delete when real messages exist
-const sampleMessage: Message = {
-  fromUserName: 'sampleName',
-  toUserName: null,
-  userId: '33333',
-  timestamp: Date.now(),
-  messageContent:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  type: MessageType.TownMessage,
-  location: {
-    x: 0,
-    y: 0,
-    rotation: 'front',
-    moving: false,
-  },
-  directMessageId: null,
-};
-
 interface ChatContainerProps {
-  // so for now- Town/Proximity will pass in undefined
   directMessageID: string | null;
   chainType: MessageType;
 }
@@ -35,8 +15,7 @@ export default function ChatContainer({
 }: ChatContainerProps): JSX.Element {
   // optional passing of an direct message chain ID, depends on chain type
   // if new DM - wait until first input to create the new DM chain -- using the id that was given as a prop
-  // eventually useCoveyAppState to connect to messages
-  const { myPlayerID, directMessageChains, townMessageChain, proximityMessageChain } = useCoveyAppState();
+  const { myPlayerID, directMessageChains, townMessageChain, proximityMessageChain, } = useCoveyAppState();
   let displayedChain: MessageChain | undefined;
   switch (chainType) {
     case 'DirectMessage': {
@@ -55,23 +34,22 @@ export default function ChatContainer({
   }
 
   if (!displayedChain) {
-    return <div className='chat-container'>
-    </div>
+    return <div className='chat-container'></div>;
   }
-  return <div className='chat-container'>
-    <div className='scrollable-messages'>
-      {
-        displayedChain.messages.map((message) => (
+  return (
+    <div className='chat-container'>
+      <div className='scrollable-messages'>
+        {displayedChain.messages.map(message => (
           <SingleMessage message={message} myPlayerID={myPlayerID} />
-        ))
-      }
+        ))}
+      </div>
+      <div className='chat-input'>
+        <ChatInput
+          messageType={chainType}
+          directMessageId={directMessageID}
+          isDisabled={undefined}
+        />
+      </div>
     </div>
-    <div className='chat-input'>
-      <ChatInput
-        messageType={chainType}
-        directMessageId={directMessageID}
-        isDisabled={undefined}
-      />
-    </div>
-  </div>;
+  );
 }
